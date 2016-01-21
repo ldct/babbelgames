f0ea74c0b95dc8821a43723fed9aa74af1b5c6c6
Li Xuanji <xuanji@gmail.com>
2016-01-21 03:29:36 -0500

    vikidia query script

---
+++ lib/vikidia.exs
@@ -0,0 +1,46 @@
+defmodule Frex.Vikidia do
+
+	def writeToFile(contents, key) do
+		File.write! "vikidia/allpages/" <> key, Poison.encode!(contents), [:write]
+		contents
+	end
+
+	def getAllPages do
+		vikidia_url = "https://fr.vikidia.org/w/api.php?action=query&list=allpages&rawcontinue=&format=json"
+
+		response = HTTPotion.get vikidia_url, [timeout: 20_000]
+		response.body
+		|> Poison.decode!
+		|> IO.inspect
+		|> writeToFile("initial")
+		|> Map.fetch!("query-continue")
+		|> Map.fetch!("allpages")
+		|> Map.fetch!("apcontinue")
+		|> getPagesStarting
+	end
+
+	def getPagesStarting(apcontinue) do
+		vikidia_url = "https://fr.vikidia.org/w/api.php?action=query&list=allpages&rawcontinue=&format=json"
+		vikidia_url = vikidia_url <> "&apcontinue=" <> URI.encode(apcontinue)
+
+		IO.inspect "hi"
+		IO.inspect vikidia_url
+
+		response = HTTPotion.get vikidia_url, [timeout: 20_000]
+
+		IO.inspect response.body
+
+		response.body
+		|> Poison.decode!
+		|> IO.inspect
+		|> writeToFile(apcontinue)
+		|> Map.fetch!("query-continue")
+		|> Map.fetch!("allpages")
+		|> Map.fetch!("apcontinue")
+		|> getPagesStarting
+	end
+
+	def printSum(a, b) do
+		IO.inspect a+b
+	end
+end
