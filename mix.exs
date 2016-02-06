50be1afce4d8765b8203ce9b30b86ecc0ae4e0b0
Li Xuanji <xuanji@quizlet.com>
2016-02-06 14:16:56 -0800

    relax version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Frex.Mixfile do
   def project do
     [app: :frex,
      version: "0.0.1",
-     elixir: "~> 1.1.1",
+     elixir: "> 1.1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
