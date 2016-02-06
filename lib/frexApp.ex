06c34b10042a7d62801821dd90b2a142ec7eb37d
Li Xuanji <xuanji@quizlet.com>
2016-02-06 14:13:33 -0800

    get port based on env

--- lib/frexApp.ex
+++ lib/frexApp.ex
@@ -4,6 +4,15 @@ defmodule FrexApp do
   def start(_type, _args) do
     IO.inspect "hello world!"
     {:ok, _} = VikidiaSentenceCache.start_link
-    Plug.Adapters.Cowboy.http FrexServer, [], port: 4000
+    Plug.Adapters.Cowboy.http FrexServer, [], port: getPort
   end
+
+  def getPort do
+    if Mix.env == :prod do
+      80
+    else
+      4000
+    end
+  end
+
 end
