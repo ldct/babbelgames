cc4cfe00b98b50580def547359609c8a8b9c2158
Li Xuanji <xuanji@gmail.com>
2016-05-18 15:41:09 +0800

    remove parsing stuff

--- lib/state.ex
+++
@@ -1,5 +0,0 @@
-defmodule SymbolicExpression.Parser.State do
-  defstruct expression: "", term: "", in_term: false, in_comment: false, paren_count: 0, result: [[]]
-
-  def new(exp), do: %__MODULE__{expression: exp}
-end
