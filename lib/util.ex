054b349158425658097fee8b907a58b147bbc7de
Li Xuanji <xuanji@gmail.com>
2016-05-23 19:14:53 +0800

    whitespace

--- lib/util.ex
+++ lib/util.ex
@@ -1,11 +1,11 @@
 defmodule Util do
-	def indexIn(element, collection) do
-	    collection
-	    |> Enum.find_index(fn e -> e == element end)
-	end
+    def indexIn(element, collection) do
+        collection
+        |> Enum.find_index(fn e -> e == element end)
+    end
 
-	def argmaxAndMax(arr) do
-	    m = Enum.max(arr)
-	    {indexIn(m, arr), m}
-	end
+    def argmaxAndMax(arr) do
+        m = Enum.max(arr)
+        {indexIn(m, arr), m}
+    end
 end
