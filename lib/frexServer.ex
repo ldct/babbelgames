ccf99c7bd77697334e9faeb34855ac6893ba8e25
Li Xuanji <xuanji@gmail.com>
2016-05-29 01:01:22 +0800

    WIP

--- lib/frexServer.ex
+++ lib/frexServer.ex
@@ -12,7 +12,7 @@ defmodule FrexServer do
 
     cacheFilename = "cache/" <> series <> "\\" <> episode
 
-    if File.exists?(cacheFilename) do
+    if false && File.exists?(cacheFilename) do
       conn
       |> put_resp_content_type("application/json")
       |> send_resp(200, File.read!(cacheFilename))
--- lib/nlp.ex
+++ lib/nlp.ex
@@ -4,6 +4,7 @@ defmodule Nlp do
             nil
         else
             str
+            |> expandShortForms
             |> String.downcase
             |> String.replace(~r/[^a-z\ ]/, "")
             |> String.replace(~r/^\ +/u, "")
@@ -25,10 +26,13 @@ defmodule Nlp do
         str
         |> String.replace("d’", "do ")
         |> String.replace("gonna", "going to")
+        |> String.replace("wanna", "want to")
         |> String.replace("’em", "them")
         |> String.replace("’til", "till")
         |> String.replace("dunno", "don't know")
         |> String.replace("gotta", "got to")
+        |> String.replace("I'm", "I am")
+        |> String.replace("there's", "there is")
     end
 
 end
