ac7f8770feab2734280616580ace20b31f528191
jamarshon <jasonl96@gmail.com>
2016-06-17 21:38:14 -0400

    Fixing navigation bar on mobile

--- frontend/js/components/NavigationBar.jsx
+++ frontend/js/components/NavigationBar.jsx
@@ -1,18 +1,64 @@
 import styles from "../../css/navigationBar.css";
 
+import $ from "jquery";
 import React from "react";
 
 var NavigationBar = React.createClass({
+  getInitialState: function() {
+    return {
+      clickedOnCross: "none",
+      desktopMode: true,
+      imgName: "/img/babbel.games.logo.png"
+    };
+  },
+
   componentDidMount: function() {
     var location = this.props.location.pathname,
         newLocation = location === "/" ? "/page/home" : location;
-    
+
     console.log(location + " " + newLocation);
-    this.props.history.push(newLocation);    
+    this.props.history.push(newLocation);
+
+    $(window).on("resize.navigationBar", this.resizeHandler);
+    this.resizeHandler();
   },
 
   returnToHomePage: function() {
     this.props.history.push("/page/home");
+  },
+
+  handleClick: function() {
+    var currentStatus = $("#" + styles.mobileNavOptions).css("display"),
+        nextStatus = currentStatus === "block" ? "none" : "block";
+
+    this.setState({clickedOnCross: nextStatus});
+  },
+
+  resizeHandler: function() {
+    this.setState(this.retrieveState());
+  },
+
+  retrieveState: function() {
+    var desktopMode = $("#" + styles.deskTopNavOptions).is(":visible"),
+        imgName = $(window).width() > 500 ? "/img/babbel.games.logo.png" : "/img/babbel.games.logo.small.png";
+    return {
+      clickedOnCross: "none",
+      desktopMode: desktopMode,
+      imgName: imgName
+    };
+  },
+
+  // Creates a dropdown for links for both the mobile and desktop versions
+  createDropDown: function(className, id, style) {
+    return (
+      <nav className={className} role="navigation">
+        <ul id={styles[id]} className={"nav navbar-nav navbar-right " + styles.navOptions} style={style}>
+          <li><a href="http://eepurl.com/b4kX5f" target="_blank">Sign-up</a></li>
+          <li><a href="mailto:xuanji@gmail.com">Contact</a></li>
+          <li><a href="#">About</a></li>
+        </ul>      
+      </nav>
+    );
   },
 
   render: function() {
@@ -20,25 +66,23 @@ var NavigationBar = React.createClass({
       <div>
         <header id={styles.header} className="navbar navbar-default">
           <div className={"container " + styles.container}>
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target=".collapse.navbar-collapse">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="close-cross"></span>
+            <div className={"navbar-header " + styles.topHeader}>
+              <button type="button" className="navbar-toggle collapsed" id={styles.dropdownButton}
+                onClick={this.handleClick} 
+                style={{display: (this.state.desktopMode ? "none" : "block") }}
+                data-toggle="collapse" data-target=".collapse.navbar-collapse">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="close-cross"></span>
               </button>
               <a id={styles.navbarBrand} className="navbar-brand brand" title="BabbelGames">
-                <img src="/img/babbel.games.logo.png" onClick={this.returnToHomePage} alt="BabbelGames"></img>
+                <img src={this.state.imgName} onClick={this.returnToHomePage} alt="BabbelGames"></img>
               </a>
             </div>
-            <nav className="collapse navbar-collapse" role="navigation">
-              <ul id={styles.navOptions} className="nav navbar-nav navbar-right">
-                <li><a href="http://eepurl.com/b4kX5f" target="_blank">Sign-up</a></li>
-                <li><a href="mailto:xuanji@gmail.com">Contact</a></li>
-                <li><a href="#">About</a></li>
-              </ul>      
-            </nav>
+            {this.createDropDown("", "deskTopNavOptions", {})}
+            {this.createDropDown("collapse navbar-collapse", "mobileNavOptions", {display: this.state.clickedOnCross})}
           </div>
         </header>
         {this.props.children}
