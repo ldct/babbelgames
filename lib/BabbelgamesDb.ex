a853195d65fb095e71f2ed2619d8b54d3294c1df
Li Xuanji <xuanji@gmail.com>
2016-06-26 16:34:12 -0700

    store correct pairs together

--- lib/BabbelgamesDb.ex
+++ lib/BabbelgamesDb.ex
@@ -1,25 +1,46 @@
 defmodule BabbelgamesDb do
 	def initDb() do
 		{:ok, pid} = Postgrex.start_link(hostname: "localhost", username: "postgres", database: "babbelgames")
+		# Postgrex.query!(pid, """
+		# 	DROP TABLE IF EXISTS users;
+		# """, [])
+		# Postgrex.query!(pid, """
+		# 	DROP TABLE IF EXISTS sessions;
+		# """, [])
+		# Postgrex.query!(pid, """
+		# 	DROP TABLE IF EXISTS correct_pairs;
+		# """, [])
+		Postgrex.query!(pid, """
+			CREATE OR REPLACE FUNCTION uniq (ANYARRAY) RETURNS ANYARRAY
+			LANGUAGE SQL
+			AS $body$
+			  SELECT ARRAY(
+			    SELECT DISTINCT $1[s.i]
+			    FROM generate_series(array_lower($1,1), array_upper($1,1)) AS s(i)
+			    ORDER BY 1
+			  );
+			$body$;
+		""", [])
 		Postgrex.query!(pid, """
 			CREATE TABLE IF NOT EXISTS users (
 				uid TEXT,
 				user_email TEXT,
-				constraint pk_users_user_email primary key (user_email)
+				constraint pk_users primary key (user_email)
 			)
 		""", [])
 		Postgrex.query!(pid, """
 			CREATE TABLE IF NOT EXISTS sessions (
 				secret TEXT,
 				user_email TEXT,
-				constraint pk_sessions_user_email primary key (user_email)
+				constraint pk_sessions primary key (user_email)
 			)
 		""", [])
 		Postgrex.query!(pid, """
 			CREATE TABLE IF NOT EXISTS correct_pairs (
 				user_email TEXT,
 				episode_md5 TEXT,
-				pairs TEXT[]
+				pairs TEXT[],
+				constraint uc_correct_pairs unique (user_email, episode_md5)
 			)
 		""", [])
 	end
@@ -28,7 +49,7 @@ defmodule BabbelgamesDb do
 		{:ok, pid} = Postgrex.start_link(hostname: "localhost", username: "postgres", database: "babbelgames")
 		Postgrex.query!(pid, """
 			INSERT INTO SESSIONS (user_email, secret) VALUES ($1, $2)
-			ON CONFLICT ON CONSTRAINT pk_sessions_user_email DO UPDATE SET secret=$2
+			ON CONFLICT ON CONSTRAINT pk_sessions DO UPDATE SET secret=$2
 		""", [user_email, secret])
 	end
 
@@ -58,6 +79,8 @@ defmodule BabbelgamesDb do
 			# TODO: create or append pair to array
 			Postgrex.query!(pid, """
 				INSERT INTO correct_pairs (user_email, episode_md5, pairs) VALUES ($1, $2, $3)
+				ON CONFLICT (user_email, episode_md5) DO UPDATE
+				SET pairs = uniq(correct_pairs.pairs || $3)
 			""", [user_email, episodeMD5, [pair]])
 		end
 
