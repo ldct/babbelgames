8a9b9fa2a6e70b497edcfaa082915626d7288903
Li Xuanji <xuanji@gmail.com>
2016-08-09 00:26:10 -0700

    Use react-bootstrap and react-bootstrap-router

--- frontend/js/components/NavigationBar.jsx
+++ frontend/js/components/NavigationBar.jsx
@@ -2,14 +2,18 @@ import styles from "../../css/navigationBar.css";
 
 import $ from "jquery";
 import React from "react";
-import {Router, Route, Link} from "react-router";
+import { Router, Route, Link } from "react-router";
+import { Navbar, NavItem, Nav, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 
 var NavigationBar = React.createClass({
   getInitialState: function() {
     return {
       clickedOnHamburger: "none",
       desktopMode: true,
-      imgName: "/img/babbel.games.logo.png"
+      imgName: "/img/babbel.games.logo.png",
+      isSmallMode: $(window).width() > 768,
     };
   },
 
@@ -31,11 +35,12 @@ var NavigationBar = React.createClass({
 
   retrieveState: function() {
     const desktopMode = $("#" + styles.deskTopNavOptions).is(":visible");
-    const imgName = $(window).width() > 500 ? "/img/babbel.games.logo.png" : "/img/babbel.games.logo.small.png";
+    const imgName = $(window).width() > 768 ? "/img/babbel.games.logo.png" : "/img/babbel.games.logo.small.png";
     return {
       clickedOnHamburger: "none",
       desktopMode: desktopMode,
-      imgName: imgName
+      imgName: imgName,
+      isSmallMode: $(window).width() < 768,
     };
   },
 
@@ -46,61 +51,66 @@ var NavigationBar = React.createClass({
   },
 
   // Creates a dropdown for links for both the mobile and desktop versions
-  createDropDown: function(className, id, style) {
+  createDropDown: function() {
     const isLoggedIn = !!window.localStorage.babbelgames_profile_image_url;
     const loggedInStyle = {display: isLoggedIn ? "block": "none"};
     const loggedInStyleInv = {display: isLoggedIn ? "none": "block"};
 
-    return (
-      <nav className={className} role="navigation">
-        <ul id={styles[id]} className={"nav navbar-nav navbar-right " + styles.navOptions} style={style}>
-          <li style={loggedInStyle}>
-            <img style={{ borderRadius: '50%' }}
-              src={window.localStorage.babbelgames_profile_image_url} />
-          </li>
+    if (isLoggedIn) return (
 
-          <li style={loggedInStyle}>
-            <a href="#" onClick={this.handleLogOut} >Log Out</a>
-          </li>
+      <Nav pullRight>
+        <NavItem href="#" style={{ padding: 0 }}>
+          <img style={{
+            borderRadius: '50%',
+            marginTop: -20,
+            marginBottom: -16,
+            display: this.state.isSmallMode ? 'none' : null,
+          }}
+            src={window.localStorage.babbelgames_profile_image_url} />
+        </NavItem>
+        <NavItem href="#" onClick={this.handleLogOut}>Log Out</NavItem>
+        <LinkContainer to="/page/uploadEpisodePair">
+          <NavItem href="#">Upload</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/page/about">
+          <NavItem href="#">About</NavItem>
+        </LinkContainer>
+      </Nav>
 
-          <li style={loggedInStyle}>
-            <Link to="/page/uploadEpisodePair">Upload</Link>
-          </li>
-
-          <li style={loggedInStyleInv}>
-            <a href="/auth/facebook">Log In</a>
-          </li>
-          <li><Link to="/page/about">About</Link></li>
-        </ul>
-      </nav>
+    );
+    else return (
+      <Nav pullRight>
+        <NavItem href="/auth/facebook">
+          Log In
+        </NavItem>
+        <LinkContainer to="/page/about">
+          <NavItem href="#">About</NavItem>
+        </LinkContainer>
+      </Nav>
     );
 
   },
 
   render: function() {
+    const isLoggedIn = !!window.localStorage.babbelgames_profile_image_url;
+
     return (
       <div>
-        <header id={styles.header} className="navbar navbar-default">
-          <div className={"container " + styles.container}>
-            <div className={"navbar-header " + styles.topHeader}>
-              <button type="button" className="navbar-toggle collapsed" id={styles.dropdownButton}
-                onClick={this.handleHamburgerClick}
-                style={{display: (this.state.desktopMode ? "none" : "block") }}
-                data-toggle="collapse" data-target=".collapse.navbar-collapse">
-                  <span className="sr-only">Toggle navigation</span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                  <span className="close-cross"></span>
-              </button>
-              <Link to="/page/home" id={styles.navbarBrand} className="navbar-brand brand" title="BabbelGames">
-                <img style={{ height: 50, }} src={this.state.imgName} alt="BabbelGames"></img>
-              </Link>
-            </div>
-            {this.createDropDown("", "deskTopNavOptions", {})}
-            {this.createDropDown("collapse navbar-collapse", "mobileNavOptions", {display: this.state.clickedOnHamburger})}
-          </div>
-        </header>
+
+      <Navbar>
+          <Navbar.Header style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Navbar.Toggle style={{ marginLeft: 0, marginRight: 0, visibility: 'hidden' }} />
+            <Link to="/page/home" title="BabbelGames">
+              <img style={{ padding: 0, height: '50px', margin: 'auto' }} src={this.state.imgName} alt="BabbelGames"></img>
+            </Link>
+          <Navbar.Toggle style={{ marginLeft: 0, marginRight: 0 }} />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            {this.createDropDown()}
+          </Navbar.Collapse>
+        </Navbar>
+
+
         {this.props.children}
       </div>
     );
