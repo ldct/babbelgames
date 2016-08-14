52d8c9eecaad36b4ec02846b1accf693a86c6da4
jamarshon <jasonl96@gmail.com>
2016-08-14 19:55:10 -0400

    Unused imports

--- frontend/js/components/EpisodeTileGalleryContainer.jsx
+++ frontend/js/components/EpisodeTileGalleryContainer.jsx
@@ -1,10 +1,7 @@
+import $ from "jquery";
 import React from "react";
-import ReactDOM from "react-dom";
-import {Router, Route, Redirect} from "react-router";
-import { browserHistory } from 'react-router';
 import EpisodeTile from "./EpisodeTile.jsx";
 import styles from "../../css/episodeTileGallery.css";
-import $ from "jquery";
 
 const EpisodeTileGallery = React.createClass({
   getInitialState: function () {
